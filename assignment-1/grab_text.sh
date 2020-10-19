300a4a871aeb58ef45bd5ea8d84fc2c0c24e1b8a
Karan Tanwar <cs5170411@iitd.ac.in>
2020-10-19 12:29:38 +0530

    Delete grab_text.sh

--- assignment-1/grab_text.sh
+++
@@ -1,4 +0,0 @@
-# mkdir ../text;
-# mkdir ../text/TaggedTrainingAP;
-for file in ./assignment1/TaggedTrainingAP/*; do sed -n "/<TEXT>/,/<\/TEXT>/p" ${file} > ./text/${file}_text ; done
-# for file in ./TaggedTrainingAP/*; do echo $file; done
